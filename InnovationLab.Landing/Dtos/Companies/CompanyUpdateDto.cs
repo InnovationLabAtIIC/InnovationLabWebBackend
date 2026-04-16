ca76b33b4bf06bfc9f738d1ddb764febcd568703
BipinSubedi0608 <subedibipin0609@gmail.com>
2026-04-16 18:13:06 +0545

    chore(company-dto): created the dtos for company entity

---
+++ InnovationLab.Landing/Dtos/Companies/CompanyUpdateDto.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using InnovationLab.Landing.Models;
+using Mapster;
+
+namespace InnovationLab.Landing.Dtos.Companies;
+
+[AdaptTo(typeof(Company))]
+public record CompanyUpdateDto
+(
+    [Required] string Name,
+    [Required] string Address,
+    [EmailAddress] string? ContactEmail,
+    [Required][Url] string LogoUrl,
+    [Range(0, int.MaxValue)] int Priority
+);
